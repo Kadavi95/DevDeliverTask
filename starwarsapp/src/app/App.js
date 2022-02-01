5450d07084b3b22b99caff4aa069716ea9b86d92
Kadavi95 <dbrzezinski1@st.swps.edu.pl>
2022-02-01 20:48:27 +0100

    added states

--- starwarsapp/src/app/App.js
+++ starwarsapp/src/app/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import '../styles/reset.css'
-import '../styles/globalStyles.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { MenuComponent } from './components/menu/MenuComponent';
-
+import React, { useState, useEffect } from "react";
+import "../styles/reset.css";
+import "../styles/globalStyles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MenuComponent } from "./components/menu/MenuComponent";
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [films, setFilms] = useState([]);
+  const [species, setSpecies] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+  const [starships, setStarships] = useState([]);
+
+  useEffect(()=> {
+    
+  }, []);
   return (
     <>
       <Router>
-        <MenuComponent/>
-          <Routes>
-            <Route></Route>
-          </Routes>
-
-        
+        <MenuComponent />
+        <Routes>
+          <Route></Route>
+        </Routes>
       </Router>
     </>
   );
